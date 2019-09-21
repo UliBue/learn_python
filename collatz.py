204ae78b1d21b054cccb04fd6cc9a57309e9e4f8
UliBue <48850219+UliBue@users.noreply.github.com>
2019-09-22 00:09:28 +1000

    added smarter output
    one line per run

--- collatz.py
+++ collatz.py
@@ -5,14 +5,17 @@ m=0
 while i<=100:
     r=0
     n=i
-    print("Test für n={}".format(n))
     while n!=1:
         if n%2==0:   # n ist gerade
             n/=2
         else:
             n=3*n+1
         r+=1        # Rundenzaehler
-    print(" bestanden nach {} Runden".format(r))
+    if r==1:
+        pf=""
+    else:
+        pf="n"
+    print("Test für n={} bestanden nach {} Runde{}.".format(i,r,pf))
     i+=1
     if m<r:
         m=r         # neue laengste Runde
